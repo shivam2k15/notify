55c9ba10b8dc72328be5ab84f550c23eab7b384b
shivam2k15 <shivamkumar2k16k@gmail.com>
2025-04-21 22:53:16 +0530

    ssl https

--- notify/services/postService.js
+++ notify/services/postService.js
@@ -1,23 +1,24 @@
+let baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const getNotifications = async () => {
-  const res = await fetch("/notifications");
+  const res = await fetch(baseUrl + "/notifications");
   if (!res.ok) throw new Error("Failed to fetch notifications");
   return res.json();
 };
 
 export const getPosts = async (page) => {
-  const res = await fetch("/post/" + page);
+  const res = await fetch(baseUrl + "/post/" + page);
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
 };
 
 export const getUserFollowers = async (userId) => {
-  const res = await fetch("/user/" + userId);
+  const res = await fetch(baseUrl + "/user/" + userId);
   if (!res.ok) throw new Error("Failed to fetch user followers");
   return res.json();
 };
 
 export const createUsers = async (payload) => {
-  const res = await fetch("/user", {
+  const res = await fetch(baseUrl + "/user", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
@@ -27,7 +28,7 @@ export const createUsers = async (payload) => {
 };
 
 export const createPosts = async (payload) => {
-  const res = await fetch("/post", {
+  const res = await fetch(baseUrl + "/post", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payload),
@@ -37,7 +38,7 @@ export const createPosts = async (payload) => {
 };
 
 export const updateNotification = async (notificationId) => {
-  const res = await fetch("/notifications", {
+  const res = await fetch(baseUrl + "/notifications", {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ notificationId }),
--- notify/src/app/page.js
+++ notify/src/app/page.js
@@ -51,7 +51,7 @@ export default function Home() {
     //  Important:  Only connect if we have the userId
     if (userId) {
       //  Initialize Socket.IO connection *once* and store it.
-      const newSocket = io("https://3.110.155.63:3000", {
+      const newSocket = io(process.env.NEXT_PUBLIC_API_BASE_URL, {
         transports: ["websocket"], // skip polling for better perf
       });
       setSocket(newSocket);
